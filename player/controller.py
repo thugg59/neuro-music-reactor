e2cb82d9d3a55d06d2144d4e2339cf21a446ed5b
thugg59 <thugg59yt@gmail.com>
2025-08-03 11:43:03 +0200

    Create controller.py

---
+++ player/controller.py
@@ -0,0 +1,14 @@
+def play_music_based_on_wave(wave):
+    """Decide artist based on EEG state."""
+    if wave == 'Delta':
+        print("Young Thug")
+    elif wave == 'Theta':
+        print("Travis Scott")
+    elif wave == 'Alpha':
+        print("Future")
+    elif wave == 'Beta':
+        print("Money Man")
+    elif wave == 'Gamma':
+       print("$uicideboy$")
+
+  
