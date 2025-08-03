1e35edb91f97b51d3bcca2d32fe358630339bd9f
thugg59 <thugg59yt@gmail.com>
2025-08-03 19:35:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from player.controller import play_music_based_on_wave
+import random
+import time
+
+waves = ['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']
+
 def main():
-    print("Neuro Music Reactor starting...")
+    print("🧠 Simulating EEG-to-artist mapping...\n")
+    while True:
+        wave = random.choice(waves)
+        print(f"Detected wave: {wave}")
+        play_music_based_on_wave(wave)
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
+
